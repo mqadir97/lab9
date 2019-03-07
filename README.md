1a11a1dbe006b5cb320cadd19023c9ae97ff1f9a
Muhammad Talha Qadir <mqadir@ucsd.edu>
2019-03-07 13:51:18 -0800

    Lab 9

---
+++ README.md
@@ -0,0 +1,4 @@
+lab9
+====
+
+Lab 9: Debugging 101
---
+++ routes/index.js
@@ -0,0 +1,21 @@
+/*
+ * GET home page.
+ */
+var labs = require('../labs.json');
+
+exports.view = function(req, res) {
+  if (labs.length < 9) {
+    var todaysLab = {
+      id: 'lab-9',
+      title: 'Debugging 101',
+      date: 'March 7',
+      image: 'debug.png'
+    };
+
+    labs.push(todaysLab);
+  }
+
+  res.render('index', {
+    labs: labs
+  });
+};
